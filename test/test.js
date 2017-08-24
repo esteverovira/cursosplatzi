5c39d5ef6e1f1684e142be581e58d7905783c0cb
esteverovira <esteve.rovira@bonarea.com>
2017-08-24 10:17:22 +0200

    Acabat modul test.js

--- test/test.js
+++ test/test.js
@@ -7,10 +7,16 @@ describe('#platzom', function(){
             expect(translation).to.equal("Program")
     })
     it('Si la palabra inicia con Z, se le añade "pe" al final',function(){
-        
+            const translation = platzom ("Zorro")
+            const translation2 = platzom("Zarpar")
+
+            expect(translation).to.equal("Zorrope")
+            expect(translation).to.equal("Zarppe")
     })
     it(' Si la palabra traducida tiene 10 o más letras, se debe partir en dos por la mitad y unir con un guión medio',function(){
-        
+            const translation = platzom ("abecedario")
+
+            expect(translation) = platzom ("abec-edario")
     })
     it('hola',function(){
         
